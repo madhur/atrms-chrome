ccfb856048b0e44a1c105b7203db538a30167579
Madhur <ahuja.madhur@gmail.com>
2014-05-14 15:21:09 +0530

    checkpoint

--- js/atrms.api.js
+++ js/atrms.api.js
@@ -4,6 +4,39 @@ function atrmsClient(EmployeeId)
 {
 	this.EmpId=EmployeeId;
 
+	var PickProps = 
+	{
+	   'StartDate': 'Week Start Date',
+	   'RouteNo': 'Route No',
+	   'Seq':'Seq',
+	   'Order':'Order',
+	   'EmpId':'Emp Id',
+	   'Name':'Name',
+	   'Days':'Days',
+	   'Zone':'Zone',
+	   'Area':'Area',
+	   'Shift':'Shift',
+	   'PickupTime':'Pickup time',
+	   'CabMatesUrl':'Roster Details'
+	};
+
+	var DropProps = 
+	{
+	   'StartDate': 'Week Start Date',
+	   'Slot': 'Parking Slot',
+	   'RouteNo': 'Route No',
+	   'Seq':'Seq',
+	   'Order':'Order',
+	   'EmpId':'Emp Id',
+	   'Name':'Name',
+	   'Days':'Days',
+	   'Zone':'Zone',
+	   'Area':'Area',
+	   'Shift':'Shift',
+	   'CabMatesUrl':'Roster Details'
+	   
+	};
+
 	var siteUrl="http://wncrpma011.japa.ad.aexp.com/TransportRoster/EmployeeReport.aspx";
 	
 
@@ -32,9 +65,7 @@ function atrmsClient(EmployeeId)
 		var EmpId = EmployeeId;
 
 		var postData= {"cmdShow": "Show", "txtEmpId" : EmpId, "txtPeopleSoft_Id": EmpId, "__VIEWSTATE":  viewState  }
-		//console.log(encodeURIComponent(viewState) );
-
-		
+				
 			$.ajax
 		({
 		    type: "POST",
@@ -56,9 +87,56 @@ function atrmsClient(EmployeeId)
 
 	var parseRosterData=function(data)
 	{
-		console.log(data);
 		
+		var pickUpDatarows=$(data).find("#MainTab tr");
+		var pickupArray=[];
+		pickUpDatarows.each(function()
+		{
+			if(this.className=="tbldata" || this.className=="tbldata1")
+			{
+				var pickupObj=new Object();
+				var columnList=$('td', this);
 
+				var i=0;
+				for(var property in PickProps)
+				{
+					pickupObj[property]=columnList[i].innerHTML;
+					i=i+1;
+
+
+				}
+				
+				pickupObj[PickProps.CabMatesUrl]=$("td > a", this).innerHTML;
+				pickupArray.push(pickupObj);
+
+			}			
+		});
+
+
+		console.log(pickupArray);
+
+		var dropDataRows=$(data).find("#MainTab2ndOrder tr");
+		var dropArray=[];
+		dropDataRows.each(function()
+		{
+			if(this.className=="tbldata" || this.className=="tbldata1")
+			{
+				var dropObj=new Object();
+				var columnList=$('td', this);
+
+				var i=0;
+				for(var property in DropProps)
+				{
+					dropObj[property]=columnList[i].innerHTML;
+					i=i+1;
+				}
+				
+				dropArray.push(dropObj);
+
+			}			
+		});
+
+		console.log(dropArray);
 
 	};
 
