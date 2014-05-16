d1d1e7a11ba633e715e7e35bfd3e58c41bd4007e
Madhur <ahuja.madhur@gmail.com>
2014-05-16 10:02:06 +0530

    updated

--- js/core.js
+++ js/core.js
@@ -9,25 +9,66 @@ $.ajaxSetup(
 
 
 
-var client=new atrmsClient('6247543');
+var empId = localStorage.getItem('empid');
+if(empId!="" && empId!=null)
+{
+	console.log(empId);
+	 $("#loading").show();
+          var client=new atrmsClient(empId);
+          client.getRosterData(completeFunc, failureFunc);
+}
 
-client.getRosterData(completeFunc, failureFunc);
+$( "#loginbutton" ).click(function() 
+{
+	var empId=$("#empid").val();
+	if(empId!="")
+	{
+
+		  localStorage.setItem("empid" ,empId);
+
+          // Notify that we saved.
+          console.log('Settings saved');
+
+          $("#loading").show();
+          var client=new atrmsClient(empId);
+          client.getRosterData(completeFunc, failureFunc);
+
+      
+
+	}
+  
+});
+
+
+
+
+
 
 function completeFunc(pickupData, dropData)
 {
 	
 	console.log(pickupData);
 	
-	var template = $('#mustache-template').html();
-    $('#moustacheHtml').append(Mustache.render(template, {
+	var pickuptemplate = $('#pickup-template').html();
+	var droptemplate = $('#drop-template').html();
+    $('#accordion').append(Mustache.render(pickuptemplate, {
         rows: pickupData
     }));
+     $('#accordion').append(Mustache.render(droptemplate, {
+        rows: dropData
+    }));
+
+    $("#loading").hide();
+
+    $(function() {
+    $( "#accordion" ).accordion();
+  });
 
 
 }
 
 function failureFunc()
 {
-
+	$("#loading").hide();
 
 }
